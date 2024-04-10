a91f7e0f58b45e5cac8147898fc0ffd1d8228a86
Clayton Karow <cj3karow@gmail.com>
2024-04-10 12:45:23 -0500

    Initial test run. go backend runs with basic hello endpoint

---
+++ back-end/main_api/main_api.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"database/sql"
+	// "encoding/json"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var monkeyDatabase *sql.DB
+
+func main() {
+	router := mux.NewRouter()
+
+	var err error
+	monkeyDatabase, err = sql.Open("sqlite3", "../database/monkey.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer monkeyDatabase.Close()
+
+	router.HandleFunc("/", hello).Methods(http.MethodPost)
+
+	http.ListenAndServe(":8000", router)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Monkey Madness Time!"))
+}
