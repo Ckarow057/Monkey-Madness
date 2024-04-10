0bd5b2e5a3bb63e5e39674d17199161353e3a557
BenAleckson <126693278+BenAleckson@users.noreply.github.com>
2024-04-10 13:21:15 -0500

    Created new pages and Started work on Encyclopedia

---
+++ front-end/Monkey-Madness-Front-End/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref, computed } from 'vue'
+import Home from './Home.vue'
+import Encyclopedia from './Encyclopedia.vue'
+import Donate from './Donate.vue'
+
+const routes = {
+  '/': Home,
+  '/encyclopedia': Encyclopedia,
+  '/donate': Donate,
+}
+
+const currentPath = ref(window.location.hash)
+const drawer = ref(false)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+</script>
+
+<template>
+  <v-app>
+    <v-navigation-drawer color="#5C4033" v-model="drawer">
+      <v-list-item prependIcon="mdi-home" href="#/" title="Home" @click="drawer = !drawer"></v-list-item>
+      <v-list-item prependIcon="mdi-monkey" href="#/encyclopedia" title="Monkey Encyclopedia"
+        @click="drawer = !drawer"></v-list-item>
+      <v-list-item prependIcon="mdi-cash-multiple" href="#/donate" title="Donate"
+        @click="drawer = !drawer"></v-list-item>
+    </v-navigation-drawer>
+    <v-app-bar color="#098205">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Monkey Madness</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <component :is="currentView"></component>
+    </v-main>
+  </v-app>
+</template>
---
+++ front-end/Monkey-Madness-Front-End/src/Encyclopedia.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import MonkeyCard from './components/MonkeyCard.vue'
+
+let id = 0;
+const monkeys = ref([
+    { id: id++, monkeyName: 'Monkey1', monkeyImage: 'placeholder.png' }
+])
+// do fetch from monkey api here
+
+</script>
+
+<template>
+    <h1>Le monk</h1>
+    <v-row no-gutters>
+        <v-col cols="6" v-for="monkey in monkeys" :key="monkey.id">
+            <MonkeyCard :titleText="monkey.monkeyName" :iamgeName="monkey.monkeyImage"></MonkeyCard>
+        </v-col>
+    </v-row>
+</template>
