e68cad9e515ba87b8136e3891957bebaec200280
Clayton Karow <cj3karow@gmail.com>
2024-04-15 12:33:08 -0500

    updated db

--- dataGen.py
+++ dataGen.py
@@ -9,20 +9,21 @@ c.execute('''CREATE TABLE monkeys
              (monkeyID INTEGER PRIMARY KEY, 
              monkeyName TEXT, 
              monkeyBreed TEXT,
+             monkeyImg TEXT, 
              monkeyFact TEXT)''')
 
 # Sample data to populate the table
 monkeys_data = [
-    (1, 'Chimpanzee', 'Pan troglodytes', 'Chimpanzees share about 98% of their DNA with humans.'),
-    (2, 'Gorilla', 'Gorilla beringei', 'Gorillas are the largest primates.'),
-    (3, 'Orangutan', 'Pongo abelii', 'Orangutans are known for their distinctive red fur.'),
-    (4, 'Bonobo', 'Pan paniscus', 'Bonobos are often referred to as pygmy chimpanzees.'),
-    (5, 'Mandrill', 'Mandrillus sphinx', 'Mandrills are the largest monkeys in the world.'),
+    (1, 'Chimpanzee', 'Pan troglodytes','TempIMG.png', 'Chimpanzees share about 98% of their DNA with humans.'),
+    (2, 'Gorilla', 'Gorilla beringei','TempIMG.png' ,'Gorillas are the largest primates.'),
+    (3, 'Orangutan', 'Pongo abelii','TempIMG.png', 'Orangutans are known for their distinctive red fur.'),
+    (4, 'Bonobo', 'Pan paniscus','TempIMG.png', 'Bonobos are often referred to as pygmy chimpanzees.'),
+    (5, 'Mandrill', 'Mandrillus sphinx','TempIMG.png', 'Mandrills are the largest monkeys in the world.'),
     # Add more data as needed
 ]
 
 # Insert the data into the table
-c.executemany('INSERT INTO monkeys VALUES (?, ?, ?, ?)', monkeys_data)
+c.executemany('INSERT INTO monkeys VALUES (?, ?, ?, ?, ?)', monkeys_data)
 
 # Commit changes and close the connection
 conn.commit()
