3335660e9598add641f92370f2395e2f73cd2537
BenAleckson <126693278+BenAleckson@users.noreply.github.com>
2024-05-01 12:46:28 -0500

    working on commit issues

--- front-end/Monkey-Madness-Front-End/src/Home.vue
+++ front-end/Monkey-Madness-Front-End/src/Home.vue
@@ -3,14 +3,15 @@ import { ref } from 'vue'
 import RandomMonkey from './components/RandomMonkey.vue';
 import MonkeyCard from './components/MonkeyCard.vue';
 
-const monkey = ref("")
+const id = 0
+const monkeys = ref("")
 
 fetch('http://localhost:8000/displaymonkey', {
     method: 'POST'
 })
     .then(response => response.json())
     .then(data => {
-        monkey.value = data
+        monkeys.value = data
         console.log(monkey)
     })
 </script>
@@ -21,8 +22,12 @@ fetch('http://localhost:8000/displaymonkey', {
     <div class="bg-green-lighten-5 text-brown-lighten-1 d-flex justify-center text-h2 pa-8">Welcome to Monkey Madness
     </div>
     <!-- add random monkey -->
-    <MonkeyCard :MonkeyName="monkey.MonkeyName" :MonkeyBreed="monkey.MonkeyImg" :MonkeyImg="monkey.MonkeyImg"
-        :MonkeyFact="monkey.MonkeyFact">
-    </MonkeyCard>
+    <v-row no-gutters>
+        <v-col cols="4" v-for="monkey in monkeys" :key="monkey.MonkeyID">
+            <RandomMonkey :MonkeyName="monkey.MonkeyName" :MonkeyBreed="monkey.MonkeyBreed"
+                :MonkeyImg="monkey.MonkeyImg" :MonkeyFact="monkey.MonkeyFact">
+            </RandomMonkey>
+        </v-col>
+    </v-row>
     <!-- footer with random things-->
 </template>
